a3af7aac2d001ac857079649a7343cfe24d64b2c
troyvedant <troyvedant@gmail.com>
2013-07-30 15:32:28 -0400

    Delete public/index.html and change root

--- config/routes.rb
+++ config/routes.rb
@@ -55,7 +55,8 @@ Tailormade::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
-  
+    
+    root :to => 'site#index'
     get 'home' => 'site#index'
-    get 'www.tailormadebarberindy.com/home' => 'site#index'
+    #get 'www.tailormadebarberindy.com/home' => 'site#index'
     end
