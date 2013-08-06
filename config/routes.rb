b47ab7cc71e11d81b1e6807eb6a1cd6a1d49c91e
troyvedant <troyvedant@gmail.com>
2013-08-06 00:04:01 -0400

    r

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Tailormade::Application.routes.draw do
+  
+  
+  root 'site#index'
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -55,8 +59,8 @@ Tailormade::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
-    get "site/index" 
-    root :to => 'site#index'
+     
+    
     #get 'home' => 'site#index'
     #get 'www.tailormadebarberindy.com/home' => 'site#index'
     end
