ac14e7bbdb74bca9329051aebb26d8b5293b1379
troyvedant <troyvedant@gmail.com>
2013-08-05 23:18:51 -0400

    route changes

--- config/routes.rb
+++ config/routes.rb
@@ -56,7 +56,7 @@ Tailormade::Application.routes.draw do
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
     
-    root :controller => 'site', :action => '/index.html.erb'
+    root :to => 'site#index'
     #get 'home' => 'site#index'
     #get 'www.tailormadebarberindy.com/home' => 'site#index'
     end
